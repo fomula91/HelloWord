50e5d1b8b5839ce792f09b8fb8af103f3948b9f3
최원영 <70950533+choewy@users.noreply.github.com>
2022-03-10 01:47:52 +0900

    코드 수정

--- routes/pages.py
+++ routes/pages.py
@@ -9,30 +9,18 @@ pages = Blueprint('pages', __name__, url_prefix='/', template_folder="templates"
 @pages.route('/')
 def home():
     [ok, _, _] = Auth.check(request)
-
-    if ok:
-        return redirect(url_for("pages.my_words"))
-
-    return render_template("index.html")
+    return redirect(url_for("pages.my_words")) if ok else render_template("index.html")
 
 
 # 나만의 단어장 화면
 @pages.route('/mywords')
 def my_words():
     [ok, _, _] = Auth.check(request)
-
-    if not ok:
-        return redirect(url_for("pages.home"))
-
-    return render_template("mywords.html")
+    return render_template("mywords.html") if ok else redirect(url_for("pages.home"))
 
 
 # 퀴즈 화면
 @pages.route('/quiz')
 def quiz():
     [ok, _, _] = Auth.check(request)
-
-    if not ok:
-        return redirect(url_for("pages.home"))
-
-    return render_template("quiz.html")
+    return render_template("quiz.html") if ok else redirect(url_for("pages.home"))
